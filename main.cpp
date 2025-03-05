3ba212665629aa8003484d84e6f2880a6a6b2113
klyg9 <kely.gbedemah24@icloud.com>
2025-03-05 23:51:24 +0000

    checkpoint 2.17 done

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
-int main() 
+using namespace std;
+int main()
 {
-  char first, middle, last;
-  first = 'K';
-  middle = 'K';
-  last = 'G';
-  std::cout << first << middle << last;
-  return 0;
+  string name, adress, phone;
+  name = "Kely kevin Gbedemah";
+  adress = "1010 31st st Newport news, VA ";
+  phone = "808-633-5055";
+  cout << name << endl;
+  cout << adress << endl;
+  cout << phone << endl;
+  return 0; 
 }
